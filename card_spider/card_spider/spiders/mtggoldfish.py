6407d2efb48f2ac57722451e3876de12984e3337
Michael Romanski <michael.romanski4@gmail.com>
2024-03-07 19:31:21 -0500

    Finished building a test spider for mttgoldfish that functions...only to find out that mtggoldfish disallows scraping on urls with price...oops. Will adapt to mtgstocks as they have no apparent restrictions listed on their robots.txt file!

--- card_spider/card_spider/spiders/mtggoldfish.py
+++ card_spider/card_spider/spiders/mtggoldfish.py
@@ -4,7 +4,13 @@ import scrapy
 class MtggoldfishSpider(scrapy.Spider):
     name = 'mtggoldfish'
     allowed_domains = ['www.mtggoldfish.com']
-    start_urls = ['http://www.mtggoldfish.com/']
+    start_urls = ['https://www.mtggoldfish.com/price/Dominaria+United/Sheoldred+the+Apocalypse#paper']
 
     def parse(self, response):
-        pass
+        current_price = response.xpath('//div[@class="price-box-paper"]//div[@class="price-box-price"]/text()').get()
+        highest_price = response.xpath('//table[@class="price-card-statistics-table price-card-statistics-table-2"]//tr[3]/td[2]/text()').get()
+
+        yield{
+            'current_price': current_price,
+            'highest_price': highest_price
+        }
